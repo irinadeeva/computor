921bd482005b5659b228d8bf9ec53a4c2de8eaa5
irinadeeva <51794769+irinadeeva@users.noreply.github.com>
2021-05-11 14:05:15 +0300

    Delete Polynomial.java

--- src/com/bhugo/computor/Polynomial.java
+++
@@ -1,9 +0,0 @@
-package com.bhugo.computor;
-
-public class Polynomial {
-    private float coefficient = 0;
-
-    Polynomial(float coefficient) {
-        this.coefficient += coefficient;
-    }
-}
